4baf6b7b8be465e1ffc4eadf772dc78f35c1fd22
wizardchen <wizardchen@tencent.com>
2025-08-10 22:06:15 +0800

    fix: Fix init tenant

--- internal/handler/test_data.go
+++ internal/handler/test_data.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"errors"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 
@@ -49,20 +47,6 @@ func (h *TestDataHandler) GetTestData(c *gin.Context) {
 	ctx := c.Request.Context()
 
 	logger.Info(ctx, "Start retrieving test data")
-
-	// Check if we're running in release/production mode
-	if gin.Mode() == gin.ReleaseMode {
-		logger.Warn(ctx, "Attempting to retrieve test data in production mode")
-		c.Error(errors.New("This API is only available in development mode"))
-		return
-	}
-
-	// Check if test data environment variables are configured
-	if os.Getenv("INIT_TEST_TENANT_ID") == "" || os.Getenv("INIT_TEST_KNOWLEDGE_BASE_ID") == "" {
-		logger.Warn(ctx, "Test data environment variables not set")
-		c.Error(errors.New("Test data not enabled"))
-		return
-	}
 
 	tenantID := uint(types.InitDefaultTenantID)
 	logger.Debugf(ctx, "Test tenant ID environment variable: %d", tenantID)
